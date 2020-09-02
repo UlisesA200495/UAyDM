3b1e5ca5d849362db22b18907bfd4b62a4bf537b
DMedina04 <70609603+DMedina04@users.noreply.github.com>
2020-09-01 22:11:38 -0300

    Program.cs mod

--- Program.cs
+++ Program.cs
@@ -10,11 +10,7 @@ namespace practicagit
     {
         static void Main(string[] args)
         {
-            
-            double n1, n2, n3, prom,i=0;
-            
-            for(i=0;i<4;i++)
-            {
+            double n1, n2, n3, prom, i = 0;
 
             Console.WriteLine("Por favor, ingrese la primera nota del alumno.");
             n1 = Convert.ToDouble(Console.ReadLine());
@@ -27,28 +23,31 @@ namespace practicagit
 
             Console.WriteLine("el promedio es de: {0} ", prom);
 
-                if(prom>=7 && prom<=10)
+            if (prom >= 7 && prom <= 10)
+            {
+                Console.WriteLine("El alumno aprobo la materia, Felicitaciones");
+            }
+            else
+            {
+                if (prom >= 4 && prom <= 7)
                 {
-                    Console.WriteLine("El alumno aprobo la materia, Felicitaciones.");
+                    Console.WriteLine("El alumno no aprobo la materia e ira a la mesa de diciembre");
                 }
                 else
                 {
-                    if (prom >= 4 && prom <= 7)
+                    if (prom >= 1 && prom <= 4)
                     {
-                        Console.WriteLine("El alumno no aprobo la materia, debera ir a mesa de examen en diciembre.");
-                    }
-                    else
-                    {
-                        if (prom >= 1 && prom <= 4)
-                        {
-                            Console.WriteLine("El alumno no aprobo la materia, debera ir a mesa de examen en diciembre.");
-                        }
+                        Console.WriteLine("El alumno no aprobo la materia e ira a la mesa de febrero");
+
+                        Console.ReadKey();
                     }
                 }
             }
 
-            Console.ReadKey();
-            
         }
+
     }
+
+
 }
+
